4b90abb5ee49c550c26d197a6a549a0dc27cddb7
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2020-12-16 19:18:49 +0900

    update: env.turn() -> player

--- evaluation.py
+++ evaluation.py
@@ -100,7 +100,7 @@ class Agent:
 
 class SoftAgent(Agent):
     def action(self, env, player, show=False):
-        p, v, self.hidden = self.planner.inference(env.observation(env.turn()), self.hidden)
+        p, v, self.hidden = self.planner.inference(env.observation(player), self.hidden)
         actions = env.legal_actions()
         prob = softmax(p, actions)
         if show:
