69db91b6204e2e90699c1285f63423792765013a
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2021-01-22 16:50:36 +0900

    chore: remove unused type_r() function from util.py

--- handyrl/train.py
+++ handyrl/train.py
@@ -25,7 +25,7 @@ import torch.distributions as dist
 import torch.optim as optim
 
 from .environment import prepare_env, make_env
-from .util import map_r, bimap_r, trimap_r, rotate, type_r
+from .util import map_r, bimap_r, trimap_r, rotate
 from .model import to_torch, to_gpu_or_not, RandomModel
 from .model import SimpleConv2DModel as DefaultModel
 from .connection import MultiProcessWorkers
