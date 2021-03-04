211c22df4fdfa387b13b0616e4725623269d0544
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2021-03-05 03:59:51 +0900

    feature: add network battle interface test

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -4,16 +4,19 @@ import random
 import traceback
 
 
+ENVS = [
+    'tictactoe',
+    'geister',
+    'parallel_tictactoe',
+]
+
+
 @pytest.fixture
 def environment_path():
     return 'handyrl.envs'
 
 
-@pytest.mark.parametrize('env', [
-    'tictactoe',
-    'geister',
-    'parallel_tictactoe',
-])
+@pytest.mark.parametrize('env', ENVS)
 def test_environment(environment_path, env):
     """Test battle loop of environments"""
     no_error_loop = False
@@ -35,3 +38,37 @@ def test_environment(environment_path, env):
         traceback.print_exc()
 
     assert no_error_loop
+
+
+@pytest.mark.parametrize('env', ENVS)
+def test_environment2(environment_path, env):
+    """Test battle loop of environments"""
+    no_error_loop = False
+    try:
+        env_path = '.'.join([environment_path, env])
+        env_module = importlib.import_module(env_path)
+        e = env_module.Environment()
+        es = {p: env_module.Environment() for p in e.players()}
+        for _ in range(100):
+            e.reset()
+            for p, e_ in es.items():
+                info = e.diff_info(p)
+                e_.update(info, True)
+            while not e.terminal():
+                actions = {}
+                for player in e.turns():
+                    action = random.choice(es[p].legal_actions(player))
+                    actions[player] = es[p].action2str(action, p)
+                actions = {p: e.str2action(a, p) for p, a in actions.items()}
+                e.steps(actions)
+                for p, e_ in es.items():
+                    info = e.diff_info(p)
+                    e_.update(info, False)
+                e.reward()
+            e.outcome()
+        no_error_loop = True
+    except Exception:
+        traceback.print_exc()
+    
+    assert no_error_loop
+
