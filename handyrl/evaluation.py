126399a4352f891d6378720d46589dfb4bc85a70
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2022-01-09 22:28:37 +0900

    feature: use load_model() function

--- handyrl/evaluation.py
+++ handyrl/evaluation.py
@@ -277,7 +277,7 @@ def network_match_acception(n, env_args, num_agents, port):
     return agents_list
 
 
-def get_model(env, model_path):
+def load_model(model_path, env):
     import torch
     from .model import ModelWrapper
     model = env.net()
@@ -290,7 +290,7 @@ def client_mp_child(env_args, model_path, conn):
     env = make_env(env_args)
     agent = build_agent(model_path, env)
     if agent is None:
-        model = get_model(env, model_path)
+        model = load_model(model_path, env)
         agent = Agent(model)
     NetworkAgentClient(agent, env, conn).run()
 
@@ -306,7 +306,8 @@ def eval_main(args, argv):
 
     agent1 = build_agent(model_path, env)
     if agent1 is None:
-        agent1 = Agent(get_model(env, model_path))
+        model = load_model(model_path, env)
+        agent1 = Agent(model)
     critic = None
 
     print('%d process, %d games' % (num_process, num_games))
