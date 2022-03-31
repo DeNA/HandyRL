0d0a8b8a300e5e5bf2d5ba75c58572c167422183
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2022-03-31 23:19:55 +0900

    feature: observation switch in agent class

--- handyrl/agent.py
+++ handyrl/agent.py
@@ -41,11 +41,12 @@ def print_outputs(env, prob, v):
 
 
 class Agent:
-    def __init__(self, model, temperature=0.0):
+    def __init__(self, model, temperature=0.0, observation=True):
         # model might be a neural net, or some planning algorithm such as game tree search
         self.model = model
         self.hidden = None
         self.temperature = temperature
+        self.observation = observation
 
     def reset(self, env, show=False):
         self.hidden = self.model.init_hidden()
@@ -74,11 +75,13 @@ class Agent:
             return random.choices(np.arange(len(p)), weights=softmax(p / self.temperature))[0]
 
     def observe(self, env, player, show=False):
-        outputs = self.plan(env.observation(player))
-        v = outputs.get('value', None)
-        if show:
-            print_outputs(env, None, v)
-        return v if v is not None else [0.0]
+        v = None
+        if self.observation:
+            outputs = self.plan(env.observation(player))
+            v = outputs.get('value', None)
+            if show:
+                print_outputs(env, None, v)
+        return v
 
 
 class EnsembleAgent(Agent):
